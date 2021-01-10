ab6bfd66424785ce8343347d9760a64a6cc9aedb
Valeriy Mikholapov <valeriy_mikholapov@epam.com>
2021-01-10 23:19:15 +0300

    Add pod files

---
+++ Example/Pods/Target Support Files/Pod_2/Pod_2-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pod_2 : NSObject
+@end
+@implementation PodsDummy_Pod_2
+@end
---
+++ Pod_2/Classes/ReplaceMe.swift
@@ -0,0 +1,3 @@
+public func printSecond() {
+    print("Second")
+}
